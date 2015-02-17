173840b678ad68859b10dba64aece4e6eedc3e95
fraenk <gitmail@frankwalter.net>
2015-02-17 14:23:53 +0100

    Change custom page template example
    Now a more useful custom page template example loading a page and it's
    children into a one-pager

--- page-custom.php
+++ page-custom.php
@@ -3,44 +3,71 @@
  Template Name: Custom Page Example
  *
  * This is your custom page template. You can create as many of these as you need.
- * Simply name is "page-whatever.php" and in add the "Template Name" title at the
+ * Simply name it "page-whatever.php" and add the "Template Name" title at the
  * top, the same way it is here.
  *
  * When you create your page, you can just select the template and viola, you have
  * a custom page template to call your very own. Your mother would be so proud.
  *
  * For more info: http://codex.wordpress.org/Page_Templates
+ * 
+ * This example creates a one-pager from a page and all it's direct child-pages
 */
 ?>
 
 <?php get_header(); ?>
 
 <body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
-
+    
     <div>
-
+        
         <?php get_template_part( 'template-parts/site-header' );  ?>
 
         <div>
 
             <div>
 
-                    <main role="main" itemprop="mainContentOfPage"> <?php //itemscope itemtype="http://schema.org/WebPageElement" ?!? ?>
+                <main role="main" itemprop="mainContentOfPage"> <?php //itemscope itemtype="http://schema.org/WebPageElement" ?!? ?>
 
-                        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-                            <?php get_template_part( 'template-parts/page-content' );  ?>
+                        
+                        <?php
+                            // first display the page content itself
+                            get_template_part( 'template-parts/page-content' );
+                            // now query for child-pages
+                            $parentPostID = get_the_ID();
+                            $args = array(
+                                'post_type' => 'page',
+                                'post_parent' => $parentPostID,
+                                'orderby' => 'menu_order',
+                                'order' => 'ASC',
+                            );
+                            $child_pages = new WP_Query( $args );
+                            // and display all child pages
+                            if( $child_pages->have_posts() ):
+                                while ( $child_pages->have_posts() ) : $child_pages->the_post();
+                                    get_template_part( 'template-parts/page-content' );
+                                endwhile;
+                            endif;
+                            // finally reset the post-data to finish rendering in the parentpage's context
+                            wp_reset_postdata();
+                        ?>
 
-                        <?php endwhile; else : ?>
+                    <?php endwhile; else : ?>
 
-                            <?php get_template_part( 'template-parts/missing-content' );  ?>
+                        <?php get_template_part( 'template-parts/missing-content' );  ?>
 
-                        <?php endif; ?>
+                    <?php endif; ?>
 
-                    </main>
+                </main>
 
-                    <?php get_sidebar(); ?>
-
+            </div>
+            
+            <div>
+                
+                <?php get_sidebar(); ?>
+                
             </div>
 
         </div>
