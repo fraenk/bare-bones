fb58cd32bf921ed6e9bfcf43e43702effa58cf2f
fraenk <gitmail@frankwalter.net>
2015-02-19 12:00:03 +0100

    Removed Page_Navi Fix Workaround
    https://core.trac.wordpress.org/query?milestone=4.1.1&group=severity&order=component
    
    The problem has been fixed in WP Core 4.1.1 - #30831

--- library/barebones.php
+++ library/barebones.php
@@ -286,8 +286,7 @@ function barebones_page_navi() {
     'next_text'    => '&rarr;',
     'type'         => 'list',
     'end_size'     => 3,
-    'mid_size'     => 3,
-    'add_args'     => false
+    'mid_size'     => 3
   ) );
   echo '</nav>';
 } /* end page navi */
